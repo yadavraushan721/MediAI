947e220b8d51a7d0fb60c7ba08b807b509af49e8
yadavraushan721 <raushankumaryadav04@gmail.com>
2026-05-21 10:50:06 +0530

    Configured environment fallback variables

--- mediai-frontend/src/api/axiosInstance.js
+++ mediai-frontend/src/api/axiosInstance.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 // Attach JWT Token Automatically
 axiosInstance.interceptors.request.use(
-  // This function runs before every request is sent to the backend. It checks if a JWT token is stored in localStorage and, if found, adds it to the Authorization header of the request. This way, we don't have to manually include the token in every API call; it will be automatically included as long as the user is logged in and has a token stored.
   (config) => {
     const token = localStorage.getItem("token");
 
